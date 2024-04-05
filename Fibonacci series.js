a175bd97e69b7e43d75cab34fa3f17285954909e
G.Gowtham <125240392+Gowtham255@users.noreply.github.com>
2024-04-05 10:31:53 +0530

    Create Fibonacci series.js

---
+++ Fibonacci series.js
@@ -0,0 +1,15 @@
+function fib(n)
+{
+    let a = 0, b = 1, c, i;
+    if( n == 0)
+        return a;
+    for(i = 2; i <= n; i++)
+    {
+    c = a + b;
+    a = b;
+    b = c;
+    }
+    return b;
+}
+let n = 15;
+console.log(fib(n));
